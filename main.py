ef8e2b033bf0d020a789accfa7382bb6b8f0b34f
elwinner08 <thomas_pruvost@live.fr>
2024-07-14 17:12:33 +0200

    ajouter du test majorité

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # C'est ici le point de départ de notre petite application Python !
 
 print('Hello Word !')
+print()
+print('Truc à la con')
+print('Quel est ton âge ?')
+age= int(input('Sinon tu risques de ne pas pouvoir boire de la bière : \n'))
+print ('Vous avez', age, 'ans.')
+if age>17 : 
+    print('Spèce dalcoolo !') 
+else :
+    print('Tu es trop jeune')
